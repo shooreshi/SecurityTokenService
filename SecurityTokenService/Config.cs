a606420d911a7777e51a2ea90873f614ca225935
Shooresh <s.golzari@gmail.com>
2018-07-29 20:46:48 +0100

    Init commit

---
+++ SecurityTokenService/Config.cs
@@ -0,0 +1,33 @@
+﻿namespace SecurityTokenService
+    {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using IdentityServer4.Models;
+
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+            {
+            return new List<ApiResource>
+                { new ApiResource("tbp", "The Bluebell Project") };
+            }
+
+        public static IEnumerable<Client> GetClients()
+            {
+            return new List<Client> {
+                new Client {
+                    ClientId = "client", 
+                // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    // secret for authentication
+                    ClientSecrets = {
+                        new Secret("secret".Sha256()) },
+                    // scopes that client has access to
+                    AllowedScopes = { "tbp" }
+                    }
+                };
+            }
+        }
+    }
